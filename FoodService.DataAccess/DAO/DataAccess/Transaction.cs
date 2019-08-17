2e5274345398dc475c7443377ebaeda201549183
johnnythemadman <bartoszmmadej@gmail.com>
2019-08-18 00:24:37 +0200

    Reorganized tier structure. Added unit test project with one Insert test

---
+++ FoodService.DataAccess/DAO/DataAccess/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FoodService.DAO.DataAccess;
+using FoodService.DAO.Database;
+
+namespace FoodService.DataAccess.DAO.DataAccess
+{
+    public static class Transaction<T>
+    {
+        //private IBaseDao<T> _source;
+        //public static void TestWithRollback(FoodServiceContext datasource, Action<Action<IBaseDao<T>>> action)
+        //{
+        //    try
+        //    {
+        //        datasource.Database.BeginTransaction();
+        //        action.Invoke;
+        //    }
+        //}
+    }
+}
---
+++ FoodService.Test/CustomerDaoTest.cs
@@ -0,0 +1,30 @@
+using FoodService.DAO.DataAccess;
+using FoodService.DAO.Database;
+using NUnit.Framework;
+
+namespace FoodService.Test
+{
+    [TestFixture]
+    public class CustomerDaoTest
+    {
+        private CustomerDao _dao = new CustomerDao();
+        private FoodServiceContext _dataSource = new FoodServiceContext();
+
+        [Test]
+        public void InsertTest()
+        {
+            var customer = new Customer
+            {
+                CustomerId = 2,
+                Email = "xd@gmail.com",
+                FirstName = "Lolek",
+                LastName = "Heheszek"
+            };
+
+            _dao.Insert(customer);
+
+            var result = _dao.SelectById(customer);
+            Assert.IsNotNull(result);
+        }
+    }
+}
